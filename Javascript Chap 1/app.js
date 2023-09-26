51d77e6619174e29595583917dbbc90821967e10
Muhammad Rameez Khan <123770847+rameezkhan77@users.noreply.github.com>
2023-09-26 22:41:58 +0500

    Add files via upload

---
+++ Javascript Chap 1/app.js
@@ -0,0 +1,12 @@
+// TASK NO 1
+alert("Welcome to our website");
+
+// Task no 2 
+alert("Error! Please enter a valid password");
+
+// Task no 3
+alert("Welcome to JS Land... \n Happy coding!")
+
+// Task no 4 
+alert("Welcome to JS Land...");
+
